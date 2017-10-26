1dd3caf8ef59577298097df9840fa14ffe91c7c4
Narendra Soni <narendra.soni@jwt.com>
2017-10-26 14:20:49 +0800

    added themese

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,15 @@
 import Typography from "typography"
 
-const typography = new Typography({ baseFontSize: '18px' })
+// custom theme
+//const typography = new Typography({ baseFontSize: '18px' })
+
+//bootstrap theme
+import bootstrapTheme from "typography-theme-bootstrap"
+const typography = new Typography(bootstrapTheme)
+
+//lawton theme
+//import lawtonTheme from "typography-theme-lawton"
+//const typography = new Typography(lawtonTheme)
+
 
 export default typography
