58725a096ae4565eb636b897f8b576672ad14437
Narendra Soni <narendra.soni@jwt.com>
2017-12-21 20:07:04 +0800

    update path

--- scripts/expect.js
+++ scripts/expect.js
@@ -4,7 +4,7 @@ set user $USER
 
 set ip "webexpressive.com"
 
-spawn ssh -oIdentityFile=/key/key.pem $user@$ip
+spawn ssh -oIdentityFile=/tmp/key.pem $user@$ip
 
 send -- "cd /var/www/html/react-site\r"
 send -- "git pull\r"
