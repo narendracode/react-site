ff35d28d533e80b1d5d19d6cdc9615d333e818b4
Narendra Soni <narendra.soni@jwt.com>
2017-12-22 21:27:38 +0800

    new production build files

--- public/path---my-files-a2c88dc2f0040fd408f4.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x93f8b10ddf38],{530:function(e,t){e.exports={data:{allFile:{edges:[{node:{relativePath:"importance-of-gitHub-for-web-developers.md",prettySize:"7.93 kB",extension:"md",birthTime:"a day ago"}}]}},pathContext:{}}}});
-//# sourceMappingURL=path---my-files-a2c88dc2f0040fd408f4.js.map
