40d76063f510854b6e56a387a667eb1bfcdd5667
Narendra Soni <narendra.soni@jwt.com>
2017-12-27 11:20:28 +0800

    new production build files

--- public/path---my-files-3639ab7352a9215ce491.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x93f8b10ddf38],{539:function(e,t){e.exports={data:{allFile:{edges:[{node:{relativePath:"importance-of-gitHub-for-web-developers.md",prettySize:"7.93 kB",extension:"md",birthTime:"a day ago"}},{node:{relativePath:"superfast-CI-CD-using-docker-travisci-and-digitalocean.md",prettySize:"18.8 kB",extension:"md",birthTime:"a day ago"}}]}},pathContext:{}}}});
-//# sourceMappingURL=path---my-files-3639ab7352a9215ce491.js.map
